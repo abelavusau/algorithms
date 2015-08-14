354b5e3a7b051edd0e04ae1f5668749e9ca9f232
Aliaksandr Belavusau <abelavusau@gmail.com>
2015-08-14 11:53:18 +0300

    updatet with 'find 2 min elements' algoritm

---
+++ src/main/java/com/abelavusau/algoritms/sorting/Max.java
@@ -0,0 +1,28 @@
+package com.abelavusau.algoritms.sorting;
+
+public class Max {
+    public static void main(String[] args) {
+	int [] a = new int [] { 6, 10, 1, 8, 5, 3, 9 };
+	int min1, min2;
+	
+	if (a[0] < a[1]) {
+	    min1 = a[0];
+	    min2 = a[1];
+	} else {
+	    min1 = a[1];
+	    min2 = a[0];
+	}
+	    
+	for (int i = 2; i < a.length; i++) {
+	    if (a[i] < min1) {
+	        min2 = min1;
+	        min1 = a[i];
+	    } else if (a[i] < min2) {
+		min2 = a[i];
+	    }
+	}
+	
+	System.out.println(min1);
+	System.out.println(min2);
+    }
+}
