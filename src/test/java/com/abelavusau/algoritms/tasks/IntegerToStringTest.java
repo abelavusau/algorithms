d386c6f34e210f198bd9e27a7372a5455ee0372f
abelavusau <abelavusau@gmail.com>
2015-12-31 22:04:48 +0300

    Added WaitOnDuplicateSet and a couple of simple algoritms

---
+++ src/test/java/com/abelavusau/algoritms/tasks/IntegerToStringTest.java
@@ -0,0 +1,12 @@
+package com.abelavusau.algoritms.tasks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegerToStringTest {
+
+	@Test
+	public void integerToStringTest() {
+		Assert.assertEquals("-2545", IntegerToString.integerToString(-2545));
+	}
+}
---
+++ src/test/java/com/abelavusau/multithreading/WaitOnDuplicateSetTest.java
@@ -0,0 +1,58 @@
+package com.abelavusau.multithreading;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaitOnDuplicateSetTest {
+
+	private WaitOnDuplicateSet<Integer> testee;
+	private HashSet<Integer> inner;
+
+	@Before
+	public void setUp() throws Exception {
+		inner = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		testee = new WaitOnDuplicateSet<Integer>(inner);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		testee.clear();
+		inner = null;
+		testee = null;
+	}
+
+	@Test
+	public void testAdd() throws InterruptedException {
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				testee.add(10);
+			}
+		});
+		
+		Thread.sleep(5000);
+		
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				testee.remove(10);
+			}
+		});
+
+		try {
+			executor.awaitTermination(1, TimeUnit.SECONDS);
+			Assert.assertEquals(11, testee.size());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
