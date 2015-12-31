5a2ba496baabdaeb7a4d41f85ced636e23a99616
abelavusau <abelavusau@gmail.com>
2015-12-31 22:04:48 +0300

    Added WaitOnDuplicateSet and a couple of simple algoritms

---
+++ src/main/java/com/abelavusau/algoritms/datastructures/BinarySearch.java
@@ -0,0 +1,38 @@
+package com.abelavusau.algoritms.datastructures;
+
+public class BinarySearch {
+	private static final int ELEMENT_NOT_FOUND = -1;
+
+	public static void main(String[] args) {
+		int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println(binarySearch(array, 7));
+	}
+
+	public static int binarySearch(int[] array, int target) {
+		return binarySearch(array, target, 0, array.length - 1);
+	}
+
+	private static int binarySearch(int[] array, int target, int left, int right) {
+		int range = right - left;
+
+		if (range == 0 && array[left] != target) {
+			return ELEMENT_NOT_FOUND;
+		}
+
+		if (array[left] > array[right]) {
+			throw new IllegalArgumentException("The array is not sorted. Please sort before search.");
+		}
+
+		int center = range / 2 + left;
+
+		if (array[center] == target) {
+			return center;
+		} else if (array[center] < target) {
+			return binarySearch(array, target, center + 1, right);
+		} else if (array[center] > target) {
+			return binarySearch(array, target, left, center - 1);
+		}
+
+		return ELEMENT_NOT_FOUND;
+	}
+}
---
+++ src/main/java/com/abelavusau/multithreading/WaitOnDuplicateSet.java
@@ -0,0 +1,107 @@
+package com.abelavusau.multithreading;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class WaitOnDuplicateSet<E> implements Set<E> {
+	private Set<E> inner;
+	private final Object lock = new Object();
+
+	public WaitOnDuplicateSet(Set<E> collection) {
+		this.inner = collection;
+	}
+
+	@Override
+	public int size() {
+		return this.inner.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.inner.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return this.inner.contains(o);
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return this.inner.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return this.inner.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return this.inner.toArray(a);
+	}
+
+	@Override
+	public boolean add(E e) {
+		boolean result = false;
+
+		synchronized (lock) {
+			while (this.inner.contains(e)) {
+				try {
+					lock.wait();
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			result = this.inner.add(e);
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		synchronized (lock) {
+			boolean result = this.inner.remove(o);
+			lock.notify();
+			return result;
+		}
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return this.inner.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		synchronized (lock) {
+			boolean result = this.inner.retainAll(c);
+			lock.notifyAll();
+			return result;
+		}
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		synchronized (lock) {
+			boolean result = this.inner.removeAll(c);
+			lock.notifyAll();
+			return result;
+		}
+	}
+
+	@Override
+	public void clear() {
+		this.inner.clear();
+		lock.notifyAll();
+	}
+}
